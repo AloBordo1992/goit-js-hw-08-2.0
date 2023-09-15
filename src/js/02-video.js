2472c53a4acb61b3ac61447479899b1c41fd774e
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-09-15 16:10:21 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+  const strigifyData = JSON.stringify(data);
+  localStorage.setItem(TIME_KEY, strigifyData);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function resumePlayback() {
+  const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
+  player.setCurrentTime(paused || 0);
+}
+resumePlayback();
